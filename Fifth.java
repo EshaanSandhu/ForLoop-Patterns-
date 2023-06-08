a9219cb6747a300de873a87ff0984f00b3f67bf2
EshaanSandhu <110472900+EshaanSandhu@users.noreply.github.com>
2023-06-08 20:11:49 +0530

    Add files via upload

---
+++ Fifth.java
@@ -0,0 +1,12 @@
+class Fifth{
+	public static void main(String args[]){
+		int i,j,number;
+		for(i=0;i<4;i++){
+			number=1;
+			for(j=0;j<=i;j++){
+			System.out.print(number+(""));
+			number++;
+			}
+			System.out.println();}
+}
+}
---
+++ Fourth.java
@@ -0,0 +1,9 @@
+class Fourth{
+	public static void main(String args[]){
+		int i,j;
+		for(i=0;i<5;i++){
+			for(j=0;j<=i;j++){
+			System.out.print(+(i+1));}
+			System.out.println();}
+}
+}
